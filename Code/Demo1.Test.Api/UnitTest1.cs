8c021c4e4ae2fc9ec9280cd308ae40b6f178b522
SabarinathanM <55434@hexaware.com>
2022-06-06 16:17:58 +0530

    Added unit test files

---
+++ Code/Demo1.Test.Api/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Demo1.Tests.Api;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
